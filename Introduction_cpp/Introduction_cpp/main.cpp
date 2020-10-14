d3030ce4a66035d3e6e4fda7cf1ca880ea0886f3
ValentinZeller <v.zeller@ludus-academie.com>
2020-10-14 15:58:15 +0200

    Exercice 77 : set_int

--- Introduction_cpp/Introduction_cpp/main.cpp
+++ Introduction_cpp/Introduction_cpp/main.cpp
@@ -1,15 +1,26 @@
 #include <iostream>
-#include "CPoint.h"
+#include "CSet_int.h"
 
 using namespace std;
 
-const int SIZE = 4;
+const int SIZE = 20;
 
 int main() {
+	int nCptEntier = 0;
+	int nVal = 0;
 
-	CPoint* tabPoint = new CPoint[SIZE];
+	CSet_int set(SIZE);
+	for (size_t i = 0; i < SIZE; i++)
+	{
+		nVal = rand() % 20;
+		if (!set.hasValue(nVal)) {
+			nCptEntier++;
+		}
+		set.addValue(nVal);
+		cout << "Valeur ajoutee au tableau : " << nVal << endl;
+	}
 
-	delete[] tabPoint;
+	cout << "Nombre d'entier different : " << nCptEntier << endl;
 
 	return 0;
 }
