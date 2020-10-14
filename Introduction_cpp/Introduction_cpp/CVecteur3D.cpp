74c117d499bfcd1da8dd8606012401bbdccb3637
ValentinZeller <v.zeller@ludus-academie.com>
2020-10-14 10:25:22 +0200

    Exercice 86 : opérateur [ ]

--- Introduction_cpp/Introduction_cpp/CVecteur3D.cpp
+++ Introduction_cpp/Introduction_cpp/CVecteur3D.cpp
@@ -1,82 +1,23 @@
 #include "CVecteur3D.h"
 
-CVecteur3D::CVecteur3D(float fltX, float fltY, float fltZ)
+float CVecteur3D::operator[](const int i) const
 {
-	this->m_fltX = fltX;
-	this->m_fltY = fltY;
-	this->m_fltZ = fltZ;
-}
-
-float CVecteur3D::getX()
-{
-	return this->m_fltX;
-}
-
-float CVecteur3D::getY()
-{
-	return this->m_fltY;
-}
-
-float CVecteur3D::getZ()
-{
-	return this->m_fltZ;
-}
-
-// Surcharges opérateurs ==
-
-bool CVecteur3D::operator==(const CVecteur3D& v)
-{
-	if (this->m_fltX == v.m_fltX && this->m_fltY == v.m_fltY && this->m_fltZ == v.m_fltZ) {
-		return true;
+	if (i >= 0 && i < SIZE) {
+		return this->v[i];
 	}
 	else {
-		return false;
+		cout << "Erreur : valeur de l'index non valide" << endl;
+		exit(1);
 	}
 }
 
-bool operator==(const CVecteur3D& v1, const CVecteur3D& v2)
+float& CVecteur3D::operator[](const int i)
 {
-	if (v1.m_fltX == v2.m_fltX && v1.m_fltY == v2.m_fltY && v1.m_fltZ == v2.m_fltZ) {
-		return true;
+	if (i >= 0 && i < SIZE) {
+		return this->v[i];
 	}
 	else {
-		return false;
+		cout << "Erreur : valeur de l'index non valide" << endl;
+		exit(1);
 	}
-}
-
-//Surcharges opérateurs !=
-
-bool CVecteur3D::operator!=(const CVecteur3D& v)
-{
-	if (this->m_fltX != v.m_fltX || this->m_fltY != v.m_fltY || this->m_fltZ != v.m_fltZ) {
-		return true;
-	}
-	else {
-		return false;
-	}
-}
-
-
-bool operator!=(const CVecteur3D& v1, const CVecteur3D& v2)
-{
-	if (v1.m_fltX != v2.m_fltX || v1.m_fltY != v2.m_fltY || v1.m_fltZ != v2.m_fltZ) {
-		return true;
-	}
-	else {
-		return false;
-	}
-}
-
-CVecteur3D operator+(const CVecteur3D& v1, const CVecteur3D& v2)
-{
-	CVecteur3D temp;
-	temp.m_fltX = v1.m_fltX + v2.m_fltX;
-	temp.m_fltY = v1.m_fltY + v2.m_fltY;
-	temp.m_fltZ = v1.m_fltZ + v2.m_fltZ;
-	return temp;
-}
-
-float operator*(const CVecteur3D& v1, const CVecteur3D& v2)
-{
-	return v1.m_fltX * v2.m_fltX + v1.m_fltY * v2.m_fltY + v1.m_fltZ * v2.m_fltZ;
 }
--- Introduction_cpp/Introduction_cpp/CVecteur3D.h
+++ Introduction_cpp/Introduction_cpp/CVecteur3D.h
@@ -1,28 +1,23 @@
 #pragma once
+
+#include <iostream>
+using namespace std;
+
+const int SIZE = 3;
+
 class CVecteur3D
 {
 private:
-	float m_fltX;
-	float m_fltY;
-	float m_fltZ;
+	float v[SIZE];
 
 public:
-	CVecteur3D(float fltX = 0, float fltY = 0, float fltZ = 0);
+	inline CVecteur3D(float fltX = 0, float fltY = 0, float fltZ = 0) {
+		this->v[0] = fltX;
+		this->v[1] = fltY;
+		this->v[2] = fltZ;
+	}
 
-	// Surcharges opérateurs ==
-	bool operator==(const CVecteur3D&); //fonction membre
-	friend bool operator==(const CVecteur3D&, const CVecteur3D&); //fonction amie
-
-	//Surcharges opérateurs !=
-	bool operator!=(const CVecteur3D&); //fonction membre
-	friend bool operator!=(const CVecteur3D&, const CVecteur3D&); //fonction amie
-
-	friend CVecteur3D operator+(const CVecteur3D&, const CVecteur3D&);
-	friend float operator*(const CVecteur3D&, const CVecteur3D&);
-
-	//Assesseurs
-	float getX();
-	float getY();
-	float getZ();
+	float operator[](const int i) const;
+	float& operator[](const int i);
 };
 
