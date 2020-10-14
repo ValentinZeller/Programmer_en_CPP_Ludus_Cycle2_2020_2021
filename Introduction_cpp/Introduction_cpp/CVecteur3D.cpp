779a668954dbb91dba572a09d7c4ebbb12637748
ValentinZeller <v.zeller@ludus-academie.com>
2020-10-14 09:56:54 +0200

    Exercice 85 : opérateurs + et *

--- Introduction_cpp/Introduction_cpp/CVecteur3D.cpp
+++ Introduction_cpp/Introduction_cpp/CVecteur3D.cpp
@@ -7,6 +7,21 @@ CVecteur3D::CVecteur3D(float fltX, float fltY, float fltZ)
 	this->m_fltZ = fltZ;
 }
 
+float CVecteur3D::getX()
+{
+	return this->m_fltX;
+}
+
+float CVecteur3D::getY()
+{
+	return this->m_fltY;
+}
+
+float CVecteur3D::getZ()
+{
+	return this->m_fltZ;
+}
+
 // Surcharges opérateurs ==
 
 bool CVecteur3D::operator==(const CVecteur3D& v)
@@ -41,6 +56,7 @@ bool CVecteur3D::operator!=(const CVecteur3D& v)
 	}
 }
 
+
 bool operator!=(const CVecteur3D& v1, const CVecteur3D& v2)
 {
 	if (v1.m_fltX != v2.m_fltX || v1.m_fltY != v2.m_fltY || v1.m_fltZ != v2.m_fltZ) {
@@ -49,4 +65,18 @@ bool operator!=(const CVecteur3D& v1, const CVecteur3D& v2)
 	else {
 		return false;
 	}
+}
+
+CVecteur3D operator+(const CVecteur3D& v1, const CVecteur3D& v2)
+{
+	CVecteur3D temp;
+	temp.m_fltX = v1.m_fltX + v2.m_fltX;
+	temp.m_fltY = v1.m_fltY + v2.m_fltY;
+	temp.m_fltZ = v1.m_fltZ + v2.m_fltZ;
+	return temp;
+}
+
+float operator*(const CVecteur3D& v1, const CVecteur3D& v2)
+{
+	return v1.m_fltX * v2.m_fltX + v1.m_fltY * v2.m_fltY + v1.m_fltZ * v2.m_fltZ;
 }
